d346039570a206e72fe3d1206fc7fb8fdfde6835
JAzzad5 <carlosjosue0114@gmail.com>
2021-10-28 15:40:09 -0600

    [ADD] Componentes de Registro, Inicio de sesion e imagenes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioSesionComponent } from './components/inicio-sesion/inicio-sesion.component';
+import { RegistroComponent } from './components/registro/registro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'inicioSesion', component: InicioSesionComponent},
+  {path: 'registro', component:RegistroComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [InicioSesionComponent, RegistroComponent]
