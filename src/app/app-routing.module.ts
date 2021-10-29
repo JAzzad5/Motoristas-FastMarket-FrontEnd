813fc9f65b613d2bbd6c24e5ccfcaf06ef85314e
JAzzad5 <carlosjosue0114@gmail.com>
2021-10-29 11:17:42 -0600

    [ADD] Añadir componentes de Landing page y registro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InicioSesionComponent } from './components/inicio-sesion/inicio-sesion.component';
 import { RegistroComponent } from './components/registro/registro.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { ContenedorPrincipalComponent } from './components/contenedor-principal/contenedor-principal.component';
 
 const routes: Routes = [
   {path:'inicioSesion', component: InicioSesionComponent},
   {path: 'registro', component:RegistroComponent},
+  {path: '', component:LandingPageComponent},
+  {path: 'inicio', component:ContenedorPrincipalComponent},
 ];
 
 @NgModule({
@@ -13,4 +17,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents = [InicioSesionComponent, RegistroComponent]
+export const routingComponents = [InicioSesionComponent, RegistroComponent,LandingPageComponent]
--- src/app/components/barra-superior/barra-superior.component.ts
+++ src/app/components/barra-superior/barra-superior.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faUser, faMapMarkerAlt, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-barra-superior',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./barra-superior.component.css']
 })
 export class BarraSuperiorComponent implements OnInit {
-
+  faUser = faUser;
+  faMapMarkerAlt = faMapMarkerAlt;
+  faShoppingCart = faShoppingCart;
   constructor() { }
 
   ngOnInit(): void {
